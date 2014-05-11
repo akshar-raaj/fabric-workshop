1b1c09addacc6e2d7a6f0c0044c70c7b492d1560
Akshar Raaj <akshar@agiliq.com>
2014-05-11 12:34:50 +0530

    Added some html content. Added css which is not being served.
    * Added html content to pages
    * Serve only two urls. Give 404 on any other
    * Add css to pages. But gunicorn can't serve the css.
    * We need nginx to serve css, add it with next commit.

--- web_application.py
+++ web_application.py
@@ -2,11 +2,24 @@ from wsgiref.simple_server import make_server
 
 def application(environ, start_response):
     path = environ.get('PATH_INFO')
+    html_start = """<html>
+                    <head>
+                    <link rel="stylesheet" href="/static/site.css">
+                    </head>
+                    <body>
+                    <p>"""
+    html_end = """</p>
+                  </body>
+                  </html>"""
+    status = "200 OK"
     if path == '/':
         response_body = "Index"
-    else:
+    elif path == '/hello':
         response_body = "Hello"
-    status = "200 OK"
+    else:
+        status = "404 Not Found"
+        response_body = "Page not found"
+    response_body = html_start + response_body + html_end
     response_headers = [("Content-Length", str(len(response_body)))]
     start_response(status, response_headers)
     return [response_body]
